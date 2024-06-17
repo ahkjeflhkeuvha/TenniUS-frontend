e0ab6f8d5d21ee98ba423eaced4c76efced5f060
오지은 <w2317@e-mirim.hs.kr>
2024-06-17 15:10:43 +0900

    data json function setting

---
+++ js/main.js
@@ -0,0 +1,109 @@
+// toggle-div 클릭 -> toggle 모양, nav-list 내려오기
+const toggleDiv = document.getElementsByClassName('nav-toggle')[0]
+const toggleI = toggleDiv.getElementsByTagName('i')[0]
+const navList = document.getElementsByClassName('nav-list')[0]
+
+toggleDiv.onclick = (event) => {
+    navList.classList.toggle('show-menu')
+
+    toggleI.classList.toggle('bi-list')
+    toggleI.classList.toggle('bi-x-lg')
+}
+
+const searchDiv = document.getElementById('search')
+const searchCon = document.getElementsByClassName('search-container')[0]
+
+searchDiv.onclick = (event) => {
+    searchCon.classList.toggle('show-search')
+}
+
+const footerSearchDiv = document.getElementById('footer-search')
+
+footerSearchDiv.onclick = (event) => {
+    searchCon.classList.toggle('show-search')
+}
+
+var target = document.querySelectorAll('.btn_open');
+var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+var targetID;
+
+// 팝업 열기
+for (var i = 0; i < target.length; i++) {
+    target[i].addEventListener('click', function () {
+        targetID = this.getAttribute('href');
+        document.querySelector(targetID).style.display = 'block';
+    });
+}
+
+// 팝업 닫기
+for (var j = 0; j < target.length; j++) {
+    btnPopClose[j].addEventListener('click', function () {
+        this.parentNode.parentNode.style.display = 'none';
+    });
+}
+
+const fileName = "data.json"
+
+fetch(fileName)
+.then((res) => res.json())
+.then((data) => setData(data))
+.catch((error) => error)
+
+let allData = ""
+
+function setData(data) {
+    allDatat = data
+    showData(data)
+}
+
+function showData(data) {
+    let itemContainerString = ""
+    let popupContainerString = ""
+
+    data.forEach((element, idx) => {
+        let articleStr = `<article class="items" id="article-${idx+1}">
+            <a href="#pop_info_${idx+1}" class="btn_open"><img src="img/${element.image}" alt=""></a>
+            <div class="explain">
+                <h2 class="item title">${element.name}</h2>
+                <h3 class="item">189,000원</h3>
+                <button class="btn cart-btn item"><a href="shopping.html">장바구니</a></button>
+            </div>
+        </article>\n`
+
+        let popupStr = `<div id="pop_info_${idx+1} class="pop_wrap" style="display:none;">
+            <div class="pop_inner">
+                <img src="img/${element.image}" alt="">
+                <h2>${element.name}</h2>
+                <h3>189,000원</h3>
+                <p>${element.description}</p>
+                <button type="button" class="btn_close">닫기</button>
+            </div>
+        </div>\n`
+
+        itemContainerString += articleStr
+        popupContainerString += popupStr
+    })
+
+    const itemContainer = document.getElementsByClassName('item-container')[0]
+    itemContainer.innerHTML = itemContainerString
+    const mainContainer = document.getElementsByClassName('popup-container')[0]
+    mainContainer.innerHTML = popupContainerString
+
+    console.log(popupContainerString)
+}
+
+function search(){
+    const searchItem = document.getElementById('search-bar').value
+    const titleList = document.getElementsByClassName('title')
+    let cnt = 0
+    
+    for(var i = 0; i<titleList.length; i++){
+        if(titleList[i].textContent.includes(searchItem)) {
+            document.getElementById(`article-${i+1}`).style.display = 'block'
+            cnt++
+        }
+        else {
+            document.getElementById(`article-${i+1}`).style.display = 'none'
+        }
+    }
+}
